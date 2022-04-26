3737f356cf275d5764431157527ad93e8dbccc25
IT20146238 <it20146238@my.sliit.lk>
2022-04-26 15:34:34 +0530

    create bill path in java bill service path

--- OnlineElectricGrid/src/main/java/com/bill/server/billService.java
+++ OnlineElectricGrid/src/main/java/com/bill/server/billService.java
@@ -1,7 +1,39 @@
 package com.bill.server;
 
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//For JSON 
+import com.google.gson.*;
+
+//For XML 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.bill.model.*;
+
+
+@Path("/Bills")
+
 public class billService {
+	
+	billModel billObj = new billModel();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readBill() {
+		return billObj.readBill();
+	}
 	
 
 
