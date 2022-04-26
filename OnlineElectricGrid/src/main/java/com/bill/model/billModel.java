c8ecc22feca122d0d5983d3c38fc0558579412ff
IT20146238 <it20146238@my.sliit.lk>
2022-04-26 13:12:55 +0530

    create database connection.

--- OnlineElectricGrid/src/main/java/com/bill/model/billModel.java
+++ OnlineElectricGrid/src/main/java/com/bill/model/billModel.java
@@ -1,7 +1,27 @@
 package com.bill.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class billModel {
 	
+	//Creating the DB connection
+		public Connection connect()
+		{
+			Connection con = null;
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+
+				
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/egsystem", "root", "2057060");
+			}
+			catch (Exception e)
+			{e.printStackTrace();}
+			return con;
+		}	
 	
 }
