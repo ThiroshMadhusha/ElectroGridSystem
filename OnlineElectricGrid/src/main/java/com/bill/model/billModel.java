a56c193db31240a7fc407ad2e80cc0130dbc9619
IT20146238 <it20146238@my.sliit.lk>
2022-04-26 13:30:03 +0530

    create insert bill statement

--- OnlineElectricGrid/src/main/java/com/bill/model/billModel.java
+++ OnlineElectricGrid/src/main/java/com/bill/model/billModel.java
@@ -22,6 +22,46 @@ public class billModel {
 			catch (Exception e)
 			{e.printStackTrace();}
 			return con;
-		}	
+		}
+		
+		public String insertBill(String account_no, String name, String address, String bill_no, String units, String t_amount )
+		{
+			String output = "";
+			try
+			 {
+			 Connection con = connect();
+			 if (con == null)
+			 	{return "Error while connecting to the database for inserting."; }
+			 
+			 // create a prepared statement
+			 
+			 
+			 String query = " insert into bills (billID,`billAccountNo`,`billName`,`billAddress`,`billNo`, `billUnit`, `billtAmount`)"+" values ( ?, ?, ?, ?, ?, ?, ?)";
+			 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 // binding values
+			 preparedStmt.setInt(1, 0); 
+			 preparedStmt.setString(2, account_no);
+			 preparedStmt.setString(3, name);
+			 preparedStmt.setString(4, address);
+			 preparedStmt.setString(5, bill_no);
+			 preparedStmt.setString(6, units);
+			 preparedStmt.setString(7, t_amount);
+			 
+			 // execute the statement
+			 
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Bill Added successfully";
+			 }
+			
+			catch (Exception e)
+			{
+				output = "Error occur during inserting";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+
 	
 }
